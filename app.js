0a248d3d9d7a6fb81263df6ba431823f14efc6b1
Mani <manikmdu@aol.com>
2018-10-04 13:22:18 +0200

    Added log messages

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
+  console.log("GET INVOKED");
+
   res.send({
     "Output": "Hello World!"
   });
 });
 
 app.post('/', function(req, res) {
+  console.log("POST INVOKED");
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello World!",
   });
 });
 
